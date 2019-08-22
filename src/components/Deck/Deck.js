9e064d8f24b3668c8a14b20a6034695be6ddd9e6
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-22 17:55:34 +0100

    Setup Deck component

---
+++ src/components/Deck/Deck.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Deck extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Lucky Cards</h1>
+      </div>
+    );
+  }
+}
+
+export default Deck;
